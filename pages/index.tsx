afa3c5563e34e4c8e1efb5e434818541ac727fa7
Breanna Holloman <breannafholloman@gmail.com>
2023-07-09 18:01:17 -0700

    Update home blog page UI
    Updated UI of home blog page to be into 3x3 rows/columns. Also, moved the date of each blog to be above the blog title rather than to the left of it

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,14 +31,14 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             {siteMetadata.description}
           </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <ul className="grid grid-cols-3 gap-4">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
               <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <div>
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
