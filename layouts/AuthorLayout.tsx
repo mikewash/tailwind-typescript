a5c22d09f93580ccf26ee79895b467e017c0f835
Huda Mahad <hudamahad123@gmail.com>
2023-07-19 21:46:31 -0400

    changes in about page

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -17,30 +17,25 @@ export default function AuthorLayout({ children, content }: Props) {
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
-          </h1>
-        </div>
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center space-x-2 pt-8">
-            <Image
-              src={avatar}
+        <div className="grid grid-rows grid-flow-col gap-24">
+          <div>
+          <Image
+              src={'/static/images/about-pic.png'} /* how to change this about me image */
               alt="avatar"
               width={192}
               height={192}
-              className="h-48 w-48 rounded-full"
+              className="h-60 w-60 rounded-full"
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
           </div>
-          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          <div>
+            <p>
+            Hi! Welcome to our blog website. This website was created by a group of MLT fellows. We're all individuals that are pursuing a career in software engineering and with an interest in web development. We've spent several weeks working together to put this website together in order to get some experience with web development. We hope you love it!
+            </p>
+          </div>
+        </div>
+        </div>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+        <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
         </div>
       </div>
     </>
