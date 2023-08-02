24a9710ebd57942750af68ef1157f33af0556479
mikewash <mikewash@gmail.com>
2023-08-01 22:25:27 -0400

    Added new library to parse body data

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from "body-parser";
 
 // Create an instance of Express
 const app = express();
 app.use(cors())
+app.use(express.json()); // For parsing JSON data
+app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded data
 
 // Define a route handler
 app.get('/', (req, res) => {
