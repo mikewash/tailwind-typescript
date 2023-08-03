19ec9ab3194984703e896b0af4eba174d84ed443
mikewash <mikewash@gmail.com>
2023-08-02 23:59:10 -0400

    Added logic to show how to use the database from the server

--- server/database.ts
+++ server/database.ts
@@ -1,8 +1,11 @@
-import {createConnection, QueryError, RowDataPacket} from 'mysql2';
+import {createConnection} from 'mysql2/promise';
 
-export const database = createConnection({
-    host: "127.0.0.1",
-    user: "test",
-    password: "mypassword",
-    database: "blog_database"
-});
+
+export const getConnection = async () => {
+    return createConnection({
+        host: "127.0.0.1",
+        user: "test",
+        password: "mypassword",
+        database: "blog_database"
+    })
+}
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import {database} from './database';
+import {getConnection} from './database';
 import bodyParser from "body-parser";
+import {getUsers} from "./model";
 
 // Create an instance of Express
 const app = express();
 app.use(cors())
 app.use(express.json()); // For parsing JSON data
 app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded data
+
 
 // Define a route handler
 app.get('/', (req, res) => {
@@ -25,16 +27,11 @@ app.get('/api', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
-app.post('/sign-in', (req, res) => {
+app.post('/sign-in', async (req, res) => {
     console.log('Body: ', req.body)
-    database.query('SELECT * FROM users', (err, rows) => {
-        if (err) {
-            console.log('Error: ', err)
-            res.status(500).json({ message: 'Internal Server Error' });
-            return;
-        }
-        res.json({ data: rows });
-    })
+    // This is just an example change this to your own logic so you can sign into the app
+    const rows = await getUsers()
+    res.json({data: rows});
 });
 
 // Start the server
