5971781a87f8040e72136a77b7d9861eaa0b9a03
Breanna Holloman <78058672+BreAmazinn@users.noreply.github.com>
2023-08-22 20:22:11 -0700

    Fonts styling css (#29)
    * UI digg.com replications
    
    Replaced "Latest" Header with Digg logo, Removed tailwind elements on the page (social media icons and verbiage), Removed "Subscribe to Newsletter". (By removing, I just commented out the portions to keep for reference)
    
    * Put nav bar in the right place under digg logo
    
    * adjusted spacing and centered logo
    
    * created "subscribe" box at the top of page and Digg logo now sends user back to homepage
    
    * added non-functional "Hide" button in subscribe box
    
    * replicated sub nav bar of "Explore", "Merch", and profile icon inline with digg homepage logo button
    
    * profile logo does not send to homepage anymore
    
    ---------
    
    Co-authored-by: mikewash <mikewash@gmail.com>

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,13 +4,13 @@
 const siteMetadata = {
   title: 'Digg.com Replica Blog',
   author: 'MLT Fellows',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Digg',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/diggLogo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
